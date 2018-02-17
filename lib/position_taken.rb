cc488a4d9502175ac585c21150fd2114b4435ac8
Salvador Delgado <sdelgado84@gmail.com>
2018-02-17 06:19:36 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 taken = nil
-def position_taken(board, index)
+def position_taken?(board, index)
   if (board[index] == " " || board[index] == "" || board[index] == nil)
     taken = false
   else
